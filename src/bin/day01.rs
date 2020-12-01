f90904c7f6c64d67beafeca8e08d590b6aea7119
David Szotten <davidszotten@gmail.com>
2020-12-01 20:49:30 +0000

    tweaks

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,4 +1,5 @@
 use aoc2020::{dispatch, Result};
+use failure::bail;
 use std::collections::HashSet;
 
 fn main() -> Result<()> {
@@ -14,61 +15,55 @@ fn part1(input: &str) -> Result<i32> {
         }
         seen.insert(n);
     }
-    Ok(0)
+    bail!("No match found");
 }
 
 fn part2(input: &str) -> Result<i32> {
-    let numbers: Vec<_> = input
+    let numbers: HashSet<_> = input
         .split('\n')
         .filter_map(|x| x.parse::<i32>().ok())
         .collect();
 
     for i in numbers.iter() {
         for j in numbers.iter() {
-            for k in numbers.iter() {
-                if i + j + k == 2020 {
-                    return Ok(i * j * k);
-                }
+            if j == i {
+                continue;
+            }
+            let missing = 2020 - i - j;
+            if numbers.contains(&missing) {
+                return Ok(i * j * missing);
             }
         }
     }
-
-    Ok(0)
+    bail!("No match found");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() -> Result<()> {
-        assert_eq!(
-            part1(
-                "1721
+    const TEST_INPUT: &str = "1721
 979
 366
 299
 675
-1456"
-            )?,
-            514579
-        );
+1456";
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        assert_eq!(part1(TEST_INPUT)?, 514579);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part1_empty() -> Result<()> {
+        assert!(part1("").is_err());
         Ok(())
     }
 
     #[test]
     fn test_part2() -> Result<()> {
-        assert_eq!(
-            part2(
-                "1721
-979
-366
-299
-675
-1456"
-            )?,
-            241861950
-        );
+        assert_eq!(part2(TEST_INPUT)?, 241861950);
         Ok(())
     }
 }
